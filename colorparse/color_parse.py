ed7e8fc1d3073e1c8aa65a169294aaf11213e319
Caleb Carithers <calebcarithers@me.com>
2021-10-28 11:32:27 -0400

    rename

--- colorparse/color_parse.py
+++ colorparse/color_parse.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import json
 
-image = Image.open('../src/images/kobosu.jpeg')
+image = Image.open('../frontend/src/images/kobosu.jpeg')
 pixels = image.load()
 width, height = image.size
 
@@ -9,7 +9,6 @@ print(f"width: {width}")
 print(f"height: {height}")
 
 img = []
-
 for y in range(height):
     img.append([])
     for x in range(width):
@@ -18,16 +17,4 @@ for y in range(height):
         img[y].append(hex_color)
 
 f = open("./kobosu.json", "w")
-
-print(len(img))
-
-for row in img:
-    row_len = len(row)
-    if row_len != width:
-        print("wrong length")
-
-# print(len(img[0]))
-
 f.write(json.dumps(img))
-
-
