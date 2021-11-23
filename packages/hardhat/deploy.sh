b3465d1e358eb92d33c2f917816b125c8bda0817
partyka1 <maciek@partyka.io>
2021-11-24 00:04:13 +0100

    yarn deploy

--- packages/hardhat/deploy.sh
+++ packages/hardhat/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-set -eux
-#echo "yarn deploy not supported"
-#echo "Please use ./tools/deploy-hardhat.sh for local deployment"
-#exit 420
+#
+# deploy.sh
+#
+# Description:
+# Deploy to local hardhat. Skips IPFS handling. Mocks img width and height
+#
+set -eu
 export DOG_IPFS_DEPLOY_BASE_URI=ipfs://local-dog/
-export DOG_IMG_WIDTH=$(( 640*2/100 ))
-export DOG_IMG_HEIGHT=$(( 480*2/100 ))
+export DOG_IMG_WIDTH=$(( 640 * 2 / 100 ))
+export DOG_IMG_HEIGHT=$(( 480 * 2 / 100 ))
 npx hardhat deploy --network localhost --export-all ../react-app/src/contracts/hardhat_contracts.json
 cp ../react-app/src/contracts/hardhat_contracts.json ../server/src/contracts/hardhat_contracts.json
