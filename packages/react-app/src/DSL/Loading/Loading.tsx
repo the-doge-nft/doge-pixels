2207ce180ce342b425a7306a853a109043580e6f
Caleb Carithers <calebcarithers@me.com>
2021-11-23 02:16:45 -0500

    loader

--- packages/react-app/src/DSL/Loading/Loading.tsx
+++ packages/react-app/src/DSL/Loading/Loading.tsx
@@ -11,9 +11,11 @@ const Loading = () => {
             <Typography variant={TVariant.PresStart18} mb={2} block>Loading</Typography>
         </Box>
         <Box __css={styles.container}>
-            {Array.from(Array(15).keys()).map((key, index) => <Box
-                __css={styles.pixel}
-            />)}
+            <Box className={"loader"}>
+                {Array.from(Array(10).keys()).map((key, index) => <Box
+                  __css={styles.pixel}
+                />)}
+            </Box>
         </Box>
     </Flex>
 }
