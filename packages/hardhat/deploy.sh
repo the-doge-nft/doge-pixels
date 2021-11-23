8fb02dab279a365cbc0365f843bcd59a7bee0c62
partyka1 <maciek@partyka.io>
2021-11-24 00:08:50 +0100

    yarn deploy

--- packages/hardhat/deploy.sh
+++ packages/hardhat/deploy.sh
@@ -6,8 +6,15 @@
 # Deploy to local hardhat. Skips IPFS handling. Mocks img width and height
 #
 set -eu
+export SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 export DOG_IPFS_DEPLOY_BASE_URI=ipfs://local-dog/
 export DOG_IMG_WIDTH=$(( 640 * 2 / 100 ))
 export DOG_IMG_HEIGHT=$(( 480 * 2 / 100 ))
-npx hardhat deploy --network localhost --export-all ../react-app/src/contracts/hardhat_contracts.json
-cp ../react-app/src/contracts/hardhat_contracts.json ../server/src/contracts/hardhat_contracts.json
+EXPORT_PATH="$SCRIPTPATH/hardhat_contracts.json"
+pushd "$SCRIPTPATH"
+  # deployed contract cannot be reused, __init() will fail
+  rm -rf ./deployments/localhost
+  npx hardhat deploy --network localhost --export-all "$EXPORT_PATH"
+  cp "$EXPORT_PATH" "$SCRIPTPATH/../react-app/src/contracts/hardhat_contracts.json"
+  cp "$EXPORT_PATH" "$SCRIPTPATH/../server/src/contracts/hardhat_contracts.json"
+popd
