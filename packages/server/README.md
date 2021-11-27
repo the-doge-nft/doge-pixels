f0cdf5b8c12ae0b62305badc4f9e49743205f814
Caleb Carithers <calebcarithers@me.com>
2021-11-27 13:57:17 -0500

    fix

---
+++ packages/server/README.md
@@ -0,0 +1,12 @@
+server dependencies
+- node.js
+- pw2
+- redis
+
+
+dev environment:
+167.172.252.56
+safeuser
+
+ssh safeuser@167.172.252.56
+
--- packages/server/src/config/vars.js
+++ packages/server/src/config/vars.js
@@ -1,8 +1,7 @@
 const path = require('path');
 
 require('dotenv-safe').config({
-  path: path.join(__dirname, '../.env'),
-  example: path.join(__dirname, '../.env.example')
+  path: path.join(__dirname, '../.env')
 });
 
 module.exports = {
