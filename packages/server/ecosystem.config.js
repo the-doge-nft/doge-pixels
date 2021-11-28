b932e84f0e3f6279ddd872c635498b394ecf4aa4
Caleb Carithers <calebcarithers@me.com>
2021-11-28 15:29:34 -0500

    fix

--- packages/server/ecosystem.config.js
+++ packages/server/ecosystem.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   apps : [{
     name   : "dog_server",
+    // cwd: "/home/nodejs/dog/source/packages/server",
     script : "./src/index.js",
-    watch: ["src"]
+    watch: ["src"],
+    env_development: {
+      NODE_ENV: 'development'
+    }
   }],
   deploy: {
     development: {
@@ -12,7 +16,8 @@ module.exports = {
       repo: "git@github.com:calebcarithers/dog.git",
       ref: "origin/feat/server",
       // "post-deploy": "echo $(pwd)"
-      "post-deploy" : "yarn install --production --frozen-lockfile && pm2 startOrRestart ecosystem.config.json --env development",
+      // "post-deploy": "pm2 --cwd /home/nodejs/dog/source/packages/server startOrRestart ecosystem.config.json --env development",
+      "post-deploy": "pm2 --cwd /home/nodejs/dog/source/packages/server start ecosystem.config.js --env development"
     }
   }
 }
