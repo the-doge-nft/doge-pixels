6ee5ce3d2a44a4540a58b4c741e2c4f38b1b80ea
Caleb Carithers <calebcarithers@me.com>
2021-11-28 17:04:19 -0500

    move some contract calls to server

--- packages/server/src/config/redis.js
+++ packages/server/src/config/redis.js
@@ -4,4 +4,7 @@ const redisClient = redis.createClient()
 redisClient.on('error', err => console.log('Redis client error', err))
 redisClient.connect()
 
-module.exports = redisClient
+const ADDRESS_TO_TOKENID = "ADDRESS_TO_TOKEN_ID"
+const SHIBA_DIMENSIONS = "SHIBA_DIMENSIONS"
+
+module.exports = {redisClient, keys: {ADDRESS_TO_TOKENID, SHIBA_DIMENSIONS}}
