a166b69be83ca2f29002cdf4dbffcf348b3083bb
Caleb Carithers <calebcarithers@me.com>
2021-11-28 14:02:41 -0500

    deploy

--- packages/server/ecosystem.config.js
+++ packages/server/ecosystem.config.js
@@ -3,5 +3,14 @@ module.exports = {
     name   : "dog_server",
     script : "./src/index.js",
     watch: ["src"]
-  }]
+  }],
+  deploy: {
+    development: {
+      user: "nodejs",
+      host: "167.172.252.56",
+      path: "/home/nodejs/dog",
+      repo: "git@github.com:calebcarithers/dog.git",
+      ref: "origin/feat/server"
+    }
+  }
 }
