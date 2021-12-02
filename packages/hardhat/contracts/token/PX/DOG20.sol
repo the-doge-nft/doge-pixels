6100b909c9755e41fe472df361a2ed0750cddbee
Caleb Carithers <calebcarithers@me.com>
2021-12-02 17:15:06 -0500

    init mock function back in

--- packages/hardhat/contracts/token/PX/DOG20.sol
+++ packages/hardhat/contracts/token/PX/DOG20.sol
@@ -14,11 +14,14 @@ contract DOG20 is ERC20Upgradeable {
     function __DOG20_init(address[] memory holders, uint256 amount) public initializer  {
         __ERC20_init("DOG20", "D20");
 
+        initMock(holders, amount);
+    }
+
+    function initMock(address[] memory holders, uint256 amount) public  {
         // give 10 pixels worth of mock dog to an address
         for (uint i = 0; i < holders.length; i++) {
             _mint(holders[i], amount);
         }
-        _mint(address(this), amount);
     }
 
 }
