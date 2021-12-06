78d6fb9f36444be5f925fe1e5206f2266b4ea1c3
Caleb Carithers <calebcarithers@me.com>
2021-12-06 15:42:09 -0500

    new copy

--- packages/react-app/src/helpers/links.ts
+++ packages/react-app/src/helpers/links.ts
@@ -4,7 +4,7 @@ import AppStore from "../store/App.store";
 export const openToEtherscanAddress = (address: string) => {
   let link = `https://etherscan.io/address/${address}`
   if (isDevModeEnabled()) {
-    link = `https://${AppStore.web3.network?.name}.etherscan.io/address/${address}`
+    link = `https://rinkeby.etherscan.io/address/${address}`
   }
   return window.open(link, "_blank")
 }
