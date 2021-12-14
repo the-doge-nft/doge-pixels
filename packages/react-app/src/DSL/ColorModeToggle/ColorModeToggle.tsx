52552fde612c09fa03e52041e7b6060a284adbd3
Caleb Carithers <calebcarithers@me.com>
2021-12-14 15:45:15 -0500

    color mode toggle drop

--- packages/react-app/src/DSL/ColorModeToggle/ColorModeToggle.tsx
+++ packages/react-app/src/DSL/ColorModeToggle/ColorModeToggle.tsx
@@ -38,38 +38,41 @@ const ColorModeToggle = (props: ColorModeToggleProps) => {
     }
   }, [colorMode])
 
-  return <Box
-    __css={styles.container}
-    onClick={toggleColorMode}
-    onMouseEnter={() => setIsDogeVisible(true)}
-    onMouseLeave={() => setIsDogeVisible(false)}
-  >
-    <motion.div
-      style={styles.handle as MotionStyle}
-      animate={{
-        left: styles.handle.left as any
-      }}
+  return <Box position={"relative"} zIndex={2}>
+    <Box
+      __css={styles.container}
+      onClick={toggleColorMode}
+      onMouseEnter={() => setIsDogeVisible(true)}
+      onMouseLeave={() => setIsDogeVisible(false)}
     >
-      <AnimatePresence>
-        {isDogeVisible && (
-          <motion.div
-            initial={{ top: "100%", position: 'relative' }}
-            animate={{ top: 0, position: 'relative' }}
-            exit={{ top: "100%", position: 'relative' }}
-          >
-            <Image
-              src={colorMode === "light" ? DarkDog : LightDog}
-              sx={styles.dogeImage}/>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </motion.div>
-    <Box __css={styles.leftIcon}>
-      <img src={PixelSun} width={"22px"} height={"22px"}/>
+      <motion.div
+        style={styles.handle as MotionStyle}
+        animate={{
+          left: styles.handle.left as any
+        }}
+      >
+        <AnimatePresence>
+          {isDogeVisible && (
+            <motion.div
+              initial={{ top: "100%", position: 'relative' }}
+              animate={{ top: 0, position: 'relative' }}
+              exit={{ top: "100%", position: 'relative' }}
+            >
+              <Image
+                src={colorMode === "light" ? DarkDog : LightDog}
+                sx={styles.dogeImage}/>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
+      <Box __css={styles.leftIcon}>
+        <img src={PixelSun} width={"22px"} height={"22px"}/>
+      </Box>
+      <Box __css={styles.rightIcon}>
+        <img src={PixelMoon} width={"22px"} height={"22px"}/>
+      </Box>
     </Box>
-    <Box __css={styles.rightIcon}>
-      <img src={PixelMoon} width={"22px"} height={"22px"}/>
-    </Box>
+    <Box __css={styles.drop}/>
   </Box>
 }
 
