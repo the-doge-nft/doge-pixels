392f2433f03fa02cec2a771b6cfc7460d2c55a1e
Caleb Carithers <calebcarithers@me.com>
2021-12-14 17:15:01 -0500

    menu

--- packages/react-app/src/DSL/Menu/Menu.style.ts
+++ packages/react-app/src/DSL/Menu/Menu.style.ts
@@ -2,7 +2,7 @@ import {Type} from "../Fonts/Fonts";
 import {colorModeType, lightOrDark} from "../Theme";
 
 const MenuStyle = {
-  parts: ["button", "list", "item", "groupTitle", "command", "divider"],
+  parts: ["button", "list", "item", "groupTitle", "command", "divider", "drop"],
   baseStyle: ({colorMode}: {colorMode: colorModeType}) => ({
     button: {
       bg: lightOrDark(colorMode, "yellow.50", "purple.700"),
@@ -12,15 +12,11 @@ const MenuStyle = {
       px: 3,
       py: 2,
       borderRadius: "30px",
-      // boxShadow: lightOrDark(colorMode, "10px 10px 0px black", "10px 10px 0px white"),
       _hover: {
         cursor: "pointer"
       },
       _active: {
         transform: "translate(4px, 4px)",
-        // boxShadow: `4px 4px 0px ${lightOrDark(colorMode,"black", "white")}`,
-        // borderBottomLeftRadius: "0px",
-        // borderBottomRightRadius: "0px"
       },
       height: "50px"
     },
@@ -45,15 +41,27 @@ const MenuStyle = {
     list: {
       py: 5,
       mt: 2,
-      // transform: "translate(px, 6px)",
       borderRadius: "0px",
       borderWidth: "1px",
       borderStyle: "solid",
       borderColor: lightOrDark(colorMode, "black", "white"),
       bg: lightOrDark(colorMode, "yellow.50", "purple.700"),
       zIndex: 2,
-      boxShadow: `6px 6px 0px ${lightOrDark(colorMode,"black", "white")}`,
+      // boxShadow: `6px 6px 0px ${lightOrDark(colorMode,"black", "white")}`,
     },
+    drop: {
+      position: "absolute",
+      w: "full",
+      h: "full",
+      bg: lightOrDark(colorMode, "black", "purple.700"),
+      top: "10px",
+      left: "10px",
+      borderRadius: "30px",
+      zIndex: -1,
+      borderWidth: "1px",
+      borderStyle: "solid",
+      borderColor: lightOrDark(colorMode, "black", "white")
+    }
   }),
   variants: {},
   defaultProps: {},
