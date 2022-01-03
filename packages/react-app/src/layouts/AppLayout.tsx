091d059e183f04bc39acbb27911ce4bf77e16948
Caleb Carithers <calebcarithers@me.com>
2022-01-03 12:32:59 -0500

    app route fix

--- packages/react-app/src/layouts/AppLayout.tsx
+++ packages/react-app/src/layouts/AppLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Box, Flex, Grid, GridItem, HStack, useColorMode, VStack} from "@chakra-ui/react";
 import Button from "../DSL/Button/Button";
 import {matchPath, useHistory, useLocation} from "react-router-dom";
-import routes, {NamedRoutes, route} from "../App.routes";
+import routes, {AppRouteInterface, NamedRoutes, route, SELECTED_PIXEL_PARAM} from "../App.routes";
 import {observer} from "mobx-react-lite";
 import AppStore from "../store/App.store";
 import ColorModeToggle from "../DSL/ColorModeToggle/ColorModeToggle";
@@ -10,7 +10,7 @@ import Link from "../DSL/Link/Link";
 import BigText from "../DSL/BigText/BigText";
 import UserMenu from "./UserMenu";
 import Typography, {TVariant} from "../DSL/Typography/Typography";
-import { Type } from "../DSL/Fonts/Fonts";
+import {Type} from "../DSL/Fonts/Fonts";
 
 interface AppLayoutProps {
   children?: any;
@@ -19,62 +19,63 @@ interface AppLayoutProps {
 const AppLayout = observer(function AppLayout({children}: AppLayoutProps) {
   return (
     <>
-    <Flex
-      w={"100vw"}
-      h={"100vh"}
-      p={{base:0, md: 8}}
-      pb={{base: 0, md: 3}}
-      flexDirection={"column"}
-    >
-      <Grid
-        templateColumns={{base: "1fr", md: "1fr 1fr 1fr", lg: "1fr 1fr 1fr", xl: "1fr 0.5fr 0.5fr"}}
-        mb={10}
-        templateRows={"1fr"}
-        display={{base: "none", md: "grid"}}
+      <Flex
+        w={"100vw"}
+        h={"100vh"}
+        p={{base: 0, md: 8}}
+        pb={{base: 0, md: 3}}
+        flexDirection={"column"}
       >
-        <GridItem w={"full"}>
-          <Flex alignItems={"center"} mb={2}>
-            <Title/>
-          </Flex>
-        </GridItem>
-        <GridItem mx={4}>
-          <Flex w={"full"} h={"full"} alignItems={"center"} justifyContent={"center"}>
-            <Nav/>
-          </Flex>
-        </GridItem>
-        <GridItem
-          display={{base: "none", md: "flex"}}
-          alignItems={"center"}
-          justifyContent={"flex-end"}
-          w={"full"}
+        <Grid
+          templateColumns={{base: "1fr", md: "1fr 1fr 1fr", lg: "1fr 1fr 1fr", xl: "1fr 0.5fr 0.5fr"}}
+          mb={10}
+          templateRows={"1fr"}
+          display={{base: "none", md: "grid"}}
         >
-          <Flex mr={8}>
-            <ColorModeToggle/>
-          </Flex>
-          {!AppStore.web3.web3Provider && <Button whiteSpace={{base: "normal", lg: "nowrap"}} onClick={() => {
-            AppStore.web3.connect()
-          }}>
-              Connect Wallet
-          </Button>}
-          <VStack>
-          {AppStore.web3.address && AppStore.web3.web3Provider && <UserMenu/>}
-          </VStack>
-        </GridItem>
-      </Grid>
-      {children}
-      {AppStore.rwd.isMobile && <MobileNav/>}
-      {!AppStore.rwd.isMobile && <Footer />}
-    </Flex>
+          <GridItem w={"full"}>
+            <Flex alignItems={"center"} mb={2}>
+              <Title/>
+            </Flex>
+          </GridItem>
+          <GridItem mx={4}>
+            <Flex w={"full"} h={"full"} alignItems={"center"} justifyContent={"center"}>
+              <HStack spacing={12}>
+                <Links/>
+              </HStack>
+            </Flex>
+          </GridItem>
+          <GridItem
+            display={{base: "none", md: "flex"}}
+            alignItems={"center"}
+            justifyContent={"flex-end"}
+            w={"full"}
+          >
+            <Flex mr={8}>
+              <ColorModeToggle/>
+            </Flex>
+            {!AppStore.web3.web3Provider && <Button whiteSpace={{base: "normal", lg: "nowrap"}} onClick={() => {
+              AppStore.web3.connect()
+            }}>
+                Connect Wallet
+            </Button>}
+            <VStack>
+              {AppStore.web3.address && AppStore.web3.web3Provider && <UserMenu/>}
+            </VStack>
+          </GridItem>
+        </Grid>
+        {children}
+        {AppStore.rwd.isMobile && <MobileNav/>}
+        {!AppStore.rwd.isMobile && <Footer/>}
+      </Flex>
     </>
   );
 });
 
 const Footer = () => {
-  const contributers: {name: string, socialLink: string}[] = [
+  const contributers: { name: string, socialLink: string }[] = [
     {name: "coldplunge", socialLink: "https://twitter.com/xcoldplunge"},
     {name: "gainormather", socialLink: "https://twitter.com/gainormather"},
     {name: "ayyyayayyy", socialLink: "https://twitter.com/ayyyayayyy"},
-    //@TODO nemo chips twitter
     {name: "nemochips", socialLink: "https://twitter.com/nemo__chips"}
   ]
   return <Box w={"full"} mt={5}>
@@ -100,38 +101,8 @@ const Footer = () => {
   </Box>
 }
 
-const Nav = () => {
-  const location = useLocation();
-  return <HStack spacing={12}>
-    {routes.map((appRoute) => {
-      if (appRoute.showOnDesktop) {
-        let path = route(appRoute.name, {
-          address: appRoute.name === NamedRoutes.DOG_PARK && AppStore.web3.address ? AppStore.web3.address : undefined
-        })
-        const match = matchPath(location.pathname, {
-          path: appRoute.path,
-          exact: true,
-          strict: false
-        })
-        console.log("debug:: test", match, location.pathname, appRoute.path)
-        return (
-          <Link
-            isNav
-            key={`desktop-nav-${appRoute.path}`}
-            to={path}
-            textDecoration={match ? "underline" : "none"}
-          >
-            {appRoute.desktopName}
-          </Link>
-        );
-      } else return null
-    })}
-  </HStack>
-}
-
 const MobileNav = observer(() => {
-  const location = useLocation()
-  const { colorMode } = useColorMode()
+  const {colorMode} = useColorMode()
   return <Flex
     bottom={0}
     zIndex={3}
@@ -143,52 +114,92 @@ const MobileNav = observer(() => {
     bg={colorMode === "light" ? "yellow.50" : "purple.700"}
     borderTopColor={colorMode === "light" ? "black" : "white"}
   >
-    {routes.map((appRoute) => {
-      if (appRoute.showOnMobile) {
-        let path = route(appRoute.name, {
-          address: appRoute.name === NamedRoutes.DOG_PARK && AppStore.web3.address ? AppStore.web3.address : undefined
-        })
-        const match = matchPath(location.pathname, {
-          path: appRoute.path,
-          exact: true,
-          strict: false
-        })
-        return (
-          <Link
-            isNav
-            to={path}
-            size={"lg"}
-            fontSize={"18px"}
-            key={`mobile-nav-${appRoute.path}`}
-            textDecoration={match ? "underline" : "none"}
-          >
-            {appRoute.mobileName}
-          </Link>
-        );
-      } else return null
-    })}
+    <Links isMobile/>
   </Flex>
 })
+
+const Links = ({isMobile}: { isMobile?: boolean }) => {
+  const location = useLocation();
+
+  const getPath = (routeName: NamedRoutes) => {
+    let path = route(routeName, {
+      address: routeName === NamedRoutes.DOG_PARK && AppStore.web3.address ? AppStore.web3.address : undefined
+    })
+    return path
+  }
+
+  const getMatch = (routePath: string) => {
+    let match = matchPath<any>(location.pathname, {
+      path: routePath,
+      exact: true,
+      strict: false
+    })
+
+    /*
+      hack for /<pixel_id> functionality. since our path for a selected pixel looks like: /:${SELECTED_PIXEL_PARAM}?
+      we need to make sure other naked routes such as '/park' do not hit a match for '/'.
+    */
+    if (match?.params && SELECTED_PIXEL_PARAM in match.params && match.params[SELECTED_PIXEL_PARAM] !== undefined && isNaN(match.params[SELECTED_PIXEL_PARAM])) {
+      return undefined
+    }
+    return match
+  }
+
+  const sortBy = (a: AppRouteInterface, b: AppRouteInterface) => {
+    const aOrder = a.order
+    const bOrder = b.order
+    if (aOrder > bOrder) {
+      return 1
+    } else if (aOrder < bOrder) {
+      return -1
+    }
+    return 0
+  }
+
+  return <>
+    {isMobile
+      ? [...routes].sort(sortBy).filter(route => route.showOnMobile).map((appRoute) => <Link
+          isNav
+          to={getPath(appRoute.name)}
+          size={"lg"}
+          fontSize={"18px"}
+          key={`mobile-nav-${appRoute.path}`}
+          textDecoration={getMatch(appRoute.path) ? "underline" : "none"}
+        >
+          {appRoute.mobileName}
+        </Link>
+      )
+      : routes.sort(sortBy).filter(route => route.showOnDesktop).map((appRoute) => <Link
+          isNav
+          key={`desktop-nav-${appRoute.path}`}
+          to={getPath(appRoute.name)}
+          textDecoration={getMatch(appRoute.path) ? "underline" : "none"}
+        >
+          {appRoute.desktopName}
+        </Link>
+      )}
+  </>
+}
 
 const Title = () => {
   const history = useHistory()
   return <Box
-      _hover={{
-        cursor: "pointer"
-      }}
-      _active={{
-        transform: "translate(4px, 4px)"
-      }}
-      onClick={() => {
-        history.push(route(NamedRoutes.VIEWER));
-      }}
-      w={"full"}
-      userSelect={"none"}
-    >
-      <BigText size={"sm"}>
-        DOGE PIXEL PORTAL
-      </BigText>
-    </Box>
+    _hover={{
+      cursor: "pointer"
+    }}
+    _active={{
+      transform: "translate(4px, 4px)"
+    }}
+    onClick={() => {
+      history.push(route(NamedRoutes.VIEWER));
+    }}
+    w={"full"}
+    userSelect={"none"}
+  >
+    <BigText size={"sm"}>
+      DOGE PIXEL PORTAL
+    </BigText>
+  </Box>
 }
 
 
