e9b3aa742c5b810291fc8cc133d1a8ee2a801992
partyka1 <maciek@partyka.io>
2022-01-31 21:17:05 +0100

    up

--- packages/hardhat/contracts/token/PX/DOG20.sol
+++ packages/hardhat/contracts/token/PX/DOG20.sol
@@ -7,17 +7,14 @@ import "../ERC20/ERC20Upgradeable.sol";
 
 contract DOG20 is ERC20Upgradeable {
 
-    constructor() {
+    constructor() {}
 
-    }
-
-    function __DOG20_init(address[] memory holders, uint256 amount) public initializer  {
+    function __DOG20_init(address[] memory holders, uint256 amount) public initializer {
         __ERC20_init("DOG20", "D20");
-
         initMock(holders, amount);
     }
 
-    function initMock(address[] memory holders, uint256 amount) public  {
+    function initMock(address[] memory holders, uint256 amount) public {
         // give 10 pixels worth of mock dog to an address
         for (uint i = 0; i < holders.length; i++) {
             _mint(holders[i], amount);
