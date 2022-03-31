2f2cdd06b8d8788d573227a385d253ab5daa4ea2
Caleb Carithers <calebcarithers@me.com>
2022-03-31 16:30:15 -0400

    no tools

--- tools/build-url-list.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for x in {0..640}
-do
-  for y in {0..480}
-  do
-    index_with_offset=$(( 1000000 + $x + $y * 640 ))
-    echo "http://143.198.55.229/ipns/k51qzi5uqu5di5wb62lm8ix9tev70ugcj8a8ikn3np2n33qnezaumg1phfzexi/metadata/pixel-$index_with_offset.json" >> urls.txt
-  done
-done
