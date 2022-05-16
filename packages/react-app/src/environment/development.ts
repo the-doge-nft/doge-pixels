66cb94a49b3719b77c7a155d69d271b30ad69114
Caleb Carithers <calebcarithers@me.com>
2022-05-16 18:24:47 -0400

    fix

--- packages/react-app/src/environment/development.ts
+++ packages/react-app/src/environment/development.ts
@@ -1,7 +1,7 @@
 const developmentEnv = {
   api: {
-    // baseURL: "https://dev.gainormather.xyz",
-    proxyURL: "http://localhost:3003"
+    baseURL: "https://dev.gainormather.xyz",
+    // proxyURL: "http://localhost:3003"
   },
   redirectAfterLogoutURL: "/",
 };
