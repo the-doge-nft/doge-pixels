59981ee626a4d3656934b2b2bb8b287f6b2e4d84
talentdev2020 <polaris.center@outlook.com>
2022-07-21 07:19:29 -0700

    fix upload image issue in twitterbot

--- packages/server/src/services/twitterBot.js
+++ packages/server/src/services/twitterBot.js
@@ -12,34 +12,6 @@ var client = new Twitter({
 	access_token_secret: access_token_secret
 });
 
-// async function getBase64ImageFromTokenURI(tokenURI) {
-//     try {
-//         const json = await axios.get(tokenURI);
-//         console.log("Token Data", json.data.image)
-//         const imageURL = json.data.image;
-//         // const image = await axios.get(imageURL, {responseType: 'arraybuffer'});
-//         // return Buffer.from(image.data).toString('base64');
-//         const image = await getBase64FromUrl(imageURL);
-//         return image;
-//     } catch(error) {
-//         sentryClient.captureMessage(`Failed to get image ${error.message}`)
-//         return '';
-//     }
-// }
-
-// const getBase64FromUrl = async (url) => {
-//     try {
-//         var request = require('request').defaults({ encoding: null });
-
-//         const response = await request.get(url);
-//         console.log({response})
-//         return "data:" + response.headers["content-type"] + ";base64," + Buffer.from(body).toString('base64');
-//     } catch (err) {
-//         console.log("Error in get base64Image " + err.message)
-//     }
-//   }
-  
-
 async function uploadImageToTwitter(tokenId, content) {
     try {
         const tokenURI = await EthersClient.PXContract.tokenURI(tokenId);
@@ -50,8 +22,8 @@ async function uploadImageToTwitter(tokenId, content) {
         var request = require('request').defaults({ encoding: null });
         request.get(imageURL, function(error, response, body) {
             if (!error && response.statusCode == 200) {
-                const base64image = "data:" + response.headers["content-type"] + ";base64," + Buffer.from(body).toString('base64');
-                console.log({base64image})
+                const base64image = Buffer.from(body).toString('base64');
+
                 client.post('media/upload', { media_data: base64image }, function (err, data, response) {
                     if (!err) {
                       let mediaId = data.media_id_string;
@@ -68,17 +40,17 @@ async function uploadImageToTwitter(tokenId, content) {
         
         
     } catch(error) {
+        console.log(error.message)
         sentryClient.captureMessage(`Failed to upload image ${error.message}`)
         return '';
     }
 }
 
 async function tweetmessage(media_id, content) {
-    console.log({content})
     client.post('statuses/update', 
         { 
             status: content, 
-            // media_ids: media_id
+            media_ids: media_id
         },
         function (err, data, response) {
             if (err) {
@@ -101,13 +73,12 @@ async function tweet() {
                     initiator = "burned";
                 }
                 
-                
                 const [x, y] = await EthersClient.PXContract.pupperToPixelCoords(tokenId);
                 const user = initiator === "minted" ? to : from;    
+                
                 let content = `Doge Pixel(${x}, ${y}) ${initiator} by ${user}`
                 content += `\n pixels.ownthedoge.com/px/${tokenId} `
-                tweetmessage(11, content);
-                // uploadImageToTwitter(tokenId, content);
+                uploadImageToTwitter(tokenId, content);
             }
         } catch (error) {
             console.log(error);
