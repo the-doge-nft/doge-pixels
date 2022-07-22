01398685cacc2fba90c7b4b960dad24c252fab07
talentdev2020 <polaris.center@outlook.com>
2022-07-22 09:58:42 -0700

    add pointer image

--- packages/server/src/services/twitterBot.js
+++ packages/server/src/services/twitterBot.js
@@ -1,6 +1,9 @@
 const ethers = require('ethers')
 const Jimp = require('jimp')
 const Twitter = require('twitter');
+const fs = require('fs')
+const { createCanvas, loadImage } = require('canvas')
+
 
 const { EthersClient } = require('../config/ethers')
 const { consumer_key, consumer_secret, access_token_key, access_token_secret } = require('../config/vars');
@@ -49,42 +52,94 @@ function generateImageObject(color) {
     }
     return jimp
 }
+function drawLine(ctx, x, y, x1, y1, x2,y2) {
+    ctx.save();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.lineTo(x,y);
+    ctx.strokeStyle = '#000000';
+    ctx.fillStyle = '000000';
+    ctx.stroke();
+        ctx.fill();
+
+    ctx.closePath();
+    // ctx.clip();
+   
+    return ctx; 
+}
 
 async function uploadImageToTwitter(tokenId, content) {
     try {
         const [x, y] = pupperToPixelCoordsLocal(tokenId)
         const color = pupperToHexLocal(tokenId);
+
+        const pointImg = await Jimp.read('src/images/pointer.png');
         Jimp.read('src/images/background.png', async function(err, image) {
+            // merge pixel image with background image
+            const nftImage = generateImageObject(color);
+            image = image.composite(nftImage, 50, 350);
+            image = image.composite(pointImg, 0, 0);
 
-        const nftImage = generateImageObject(color);
-        image = image.composite(nftImage, 50, 350);
-
-        const font = await Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
-        image.print(font, 61, 445, `(${x}, ${y})`);
-        // image.write('a.png');
-
-        image.getBase64('image/png', function(error, base64image) {
-            if (!error) {
-                base64image = base64image.replace('data:image/png;base64,', '');
-                
-                client.post('media/upload', { media_data: base64image }, function (err, data, response) {
-                    if (!err) {
-                        let mediaId = data.media_id_string;
-                        tweetmessage(mediaId, content);
-                    } else {
-                        console.log(`Error occured uploading image\t${err}`);
-                    }
-                    });
-            } else {
-                tweetmessage(0, content);
-                console.log(`Failed to get base64Image ${error.message}`);
-            }
-        });
-    })
+            // add coordinates
+            const font = await Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
+            image.print(font, 61, 445, `(${x}, ${y})`);
+            // get base64 image
+            image.getBase64('image/png', function(error, base64image) {
+                if (!error) {
+                    base64image = base64image.replace('data:image/png;base64,', '');
+                    
+                    // upload image to twitter
+                    client.post('media/upload', { media_data: base64image }, function (err, data, response) {
+                        if (!err) {
+                            let mediaId = data.media_id_string;
+                            tweetmessage(mediaId, content);
+                        } else {
+                            console.log(`Error occured uploading image\t${err}`);
+                        }
+                        });
+                } else {
+                    tweetmessage(0, content);
+                    console.log(`Failed to get base64Image ${error.message}`);
+                }
+            });
+        })
     } catch(error) {
         console.log(error.message)
         sentryClient.captureMessage(`Failed to upload image ${error.message}`)
         return '';
+    }
+}
+function addPointerImage(tokenId, content) {
+    try {
+        const [x, y] = pupperToPixelCoordsLocal(tokenId)
+
+        const canvas = createCanvas(640, 480)
+        let context = canvas.getContext('2d')
+        let x1, y1, x2, y2;
+        if (y >= 350 || (x >= 320 && y >= 240)) {
+            x1 = 140;
+            y1 = 370;
+            x2 = 140;
+            y2 = 380;
+        } else {
+            x1 = 70;
+            y1 = 350;
+            x2 = 80;
+            y2 = 350;
+        }
+        context = drawLine(context, x, y, x1, y1, x2, y2);
+        
+        // loadImage('src/images/back.png').then(image => {
+            // context.drawImage(image, 0, 0, 640, 640)
+            const buffer = canvas.toBuffer('image/png')
+            fs.writeFile('src/images/pointer.png', buffer, "", function () {
+                uploadImageToTwitter(tokenId, content);
+            })
+        //   })
+     } catch(error) {
+        console.log(error.message)
     }
 }
 
@@ -120,7 +175,7 @@ async function tweet() {
                 let content = `Doge Pixel(${x}, ${y}) ${initiator} by ${user}`
                 content += `\n pixels.ownthedoge.com/px/${tokenId}`
 
-                uploadImageToTwitter(tokenId, content);
+                addPointerImage(tokenId, content);
             }
         } catch (error) {
             console.log(error);
