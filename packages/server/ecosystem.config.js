e907635fe59ee869aeb50a2c2bec2d35148c7e8f
Caleb Carithers <calebcarithers@me.com>
2022-07-26 13:42:43 -0400

    lets push the mem a bit

--- packages/server/ecosystem.config.js
+++ packages/server/ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
     name   : "dog_server",
     script : "./src/index.js",
     watch: ["src"],
-    ignore_watch: ["./combined.log", './src/assets/images']
+    ignore_watch: ["./combined.log", './src/assets/images'],
+    max_memory_restart: '750M'
   }],
   deploy: {
     development: {
