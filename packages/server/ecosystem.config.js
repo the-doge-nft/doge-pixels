567c622815565068311ec9947c2d2fbc9940cd66
Caleb Carithers <calebcarithers@me.com>
2022-07-29 11:09:36 -0400

    up

--- packages/server/ecosystem.config.js
+++ packages/server/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name   : "dog_server",
     script : "./src/index.js",
-    watch: "false",
+    watch: false,
     ignore_watch: ["./combined.log", './src/assets/images', 'src/assets/images/pointer.png'],
     max_memory_restart: '750M',
     env_development: {
