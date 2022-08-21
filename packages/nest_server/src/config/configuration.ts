a71d3304ca3dda59fce8f16fd58c793d948672b8
Caleb Carithers <calebcarithers@me.com>
2022-08-20 20:50:07 -0400

    init nest

---
+++ packages/nest_server/src/config/configuration.ts
@@ -0,0 +1,23 @@
+export default () => ({
+  port: parseInt(process.env.PORT) || 3000,
+  appEnv: process.env.APP_ENV || 'development',
+  infura: {
+    projectId: process.env.INFURA_PROJECT_ID,
+    secret: process.env.INFURA_SECRET,
+    httpEndpoint: process.env.INFURA_HTTP_ENDPOINT,
+    wsEndpoint: process.env.INFURA_WS_ENDPOINT,
+  },
+  sentryDns: process.env.SENTRY_DNS,
+  pixelContractDeploymentBlockNumber:
+    parseInt(process.env.CONTRACT_BLOCK_NUMBER_DEPLOYMENT) || 0,
+  twitter: {
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  },
+  discord: {
+    secret: process.env.DISCORD_SECRET,
+    channelId: process.env.DISCORD_CHANNEL_ID,
+  },
+});
---
+++ packages/nest_server/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(app.get(ConfigService).get('PORT'));
+}
+bootstrap();
