32412f975005a3d2ee21e8111dad7e28ffa5db3b
Caleb Carithers <calebcarithers@me.com>
2022-08-26 19:35:45 -0400

    up

--- packages/ciserver/run-deployment.sh
+++ packages/ciserver/run-deployment.sh
@@ -4,7 +4,7 @@ export DEPLOYMENT_ENV="$2"
 
 
 export SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-echo 'export BUILD_ID=" '$BUILD_ID'"' > ~/.pixels-envs
+echo 'export BUILD_ID="'$BUILD_ID'"' > ~/.pixels-envs
 echo 'export DOCKER_REGISTRY="calebcarithers"' >> ~/.pixels-envs
 echo 'export LAST_DEPLOYMENT_AT="'$(date)'"' >> ~/.pixels-envs
 
