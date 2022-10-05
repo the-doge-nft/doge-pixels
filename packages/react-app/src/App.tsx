60862c51ac6056b88a4519264a926e3bac745283
Caleb Carithers <calebcarithers@me.com>
2022-10-05 02:57:29 -0500

    upp

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -24,7 +24,7 @@ const logAppVersionToConsole = () => {
 
 AppStore.init()
 
-console.log('TEST DEV DEPLOY AGAIN')
+console.log('TEST DEV DEPLOY AGGGAIN')
 
 function App() {
   useEffect(logAppVersionToConsole, [])
