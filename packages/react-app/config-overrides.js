816f6fdcbf0620bb9fdb737890458c8d68b9db06
Caleb Carithers <calebcarithers@me.com>
2022-11-01 03:50:21 -0500

    webpack 5 🤝 wallconnect hell

--- packages/react-app/config-overrides.js
+++ packages/react-app/config-overrides.js
@@ -1,13 +1,41 @@
 // config-overrides.js
-const { override } = require("customize-cra");
+// const { override } = require("customize-cra");
+// const supportMjs = () => webpackConfig => {
+//   webpackConfig.module.rules.push({
+//     test: /\.mjs$/,
+//     include: /node_modules/,
+//     type: "javascript/auto",
+//   });
+//   return webpackConfig;
+// };
 
-const supportMjs = () => webpackConfig => {
-  webpackConfig.module.rules.push({
-    test: /\.mjs$/,
-    include: /node_modules/,
-    type: "javascript/auto",
-  });
-  return webpackConfig;
-};
 
-module.exports = override(supportMjs());
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+  const fallback = config.resolve.fallback || {};
+  Object.assign(fallback, {
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "assert": require.resolve("assert"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "os": require.resolve("os-browserify"),
+    "url": require.resolve("url")
+  })
+  config.ignoreWarnings = [/Failed to parse source map/];
+  config.resolve.fallback = fallback;
+  config.plugins = (config.plugins || []).concat([
+  new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ])
+  config.module.rules.push({
+    test: /\.m?js/,
+    resolve: {
+        fullySpecified: false
+    }
+})
+  return config;
+}
