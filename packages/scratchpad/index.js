9eb809e84b878c1f93488447a81b5e8c3afa9ec4
Caleb Carithers <calebcarithers@me.com>
2022-11-02 18:33:26 -0500

    update sochain dogecoin sync

--- packages/scratchpad/index.js
+++ packages/scratchpad/index.js
@@ -1,14 +1,25 @@
 const ethers = require("ethers")
+const axios = require("axios")
 
 
 const main = async () => {
-    const address = "0xA2fE1d4145443EbC027F15C9F7ca27f7e9FD5F33"
-    const otheraddress = "0xa2fe1d4145443ebc027f15c9f7ca27f7e9fd5f33"
-    const provider = ethers.getDefaultProvider()
-    const name = await provider.lookupAddress(address)
-    const otherName = await provider.lookupAddress(address)
-    console.log("name", name)
-    console.log("other name", otherName)
+    // const address = "0xA2fE1d4145443EbC027F15C9F7ca27f7e9FD5F33"
+    // const otheraddress = "0xa2fe1d4145443ebc027f15c9f7ca27f7e9fd5f33"
+    // const provider = ethers.getDefaultProvider()
+    // const name = await provider.lookupAddress(address)
+    // const otherName = await provider.lookupAddress(address)
+
+    const { data } = await axios.get("https://chain.so/api/v2/address/doge/D8HjKf37rF3Ho7tjwe17MPN8xQ2UbHSUhB")
+    const txs = data?.data?.txs
+    console.log(JSON.stringify(txs, undefined, 2))
+    console.log(txs.length)
+
+
+    // const { data } = await axios.get("https://chain.so/api/v2/get_tx_received/doge/D8HjKf37rF3Ho7tjwe17MPN8xQ2UbHSUhB/20af2fa48cc3e8cff6e2c75ad67ea3046e45a9510b43cea6adee79845da2db8e")
+    // console.log(data?.data?.txs)
+
+    // console.log("name", name)
+    // console.log("other name", otherName)
     return
 }
 
