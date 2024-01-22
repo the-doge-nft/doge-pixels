ba00951478536262fd9c52d1418eac0901692bc0
caleb <calebcarithers@me.com>
2024-01-22 03:26:37 -0600

    up

--- packages/react-app/src/environment/helpers.ts
+++ packages/react-app/src/environment/helpers.ts
@@ -1,6 +1,11 @@
 const isProduction = () => {
   return process.env.NODE_ENV === "production" && process.env.NEXT_PUBLIC_DOG_ENV === "production";
 };
+
+console.log(
+  process.env.NODE_ENV,
+  process.env.NEXT_PUBLIC_DOG_ENV
+)
 const isStaging = () => {
   return process.env.NODE_ENV === "production" && process.env.NEXT_PUBLIC_DOG_ENV === "staging";
 };
